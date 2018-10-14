e05a5b21f9c854b92af75c76d52443b340e65829
Dan Murray <dqm8494@rit.edu>
2018-10-14 16:01:34 -0400

    Yo dawg its all fixed

--- app/src/main/java/com/example/salah/swen_101/ViewMenu.java
+++ app/src/main/java/com/example/salah/swen_101/ViewMenu.java
@@ -2,6 +2,11 @@ package com.example.salah.swen_101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ViewMenu extends AppCompatActivity {
 
@@ -9,5 +14,7 @@ public class ViewMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_menu);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
     }
 }
