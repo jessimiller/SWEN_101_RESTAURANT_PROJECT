978b595c73051abcdfb7af610d1a64cdb93d57b0
jessi <jcm9472@rit.edu>
2018-10-14 11:58:18 -0400

    customerhome button functionality implemented

--- app/src/main/java/com/example/salah/swen_101/MainActivity.java
+++ app/src/main/java/com/example/salah/swen_101/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.salah.swen_101;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.CustomerButton);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openCustomerHome();
+            }
+        });
+    }
+    public void openCustomerHome(){
+        Intent intent = new Intent(this, CustomerHome.class);
+        startActivity(intent);
     }
 }
