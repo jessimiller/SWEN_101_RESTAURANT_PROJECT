bc22c28ba4fad2cb8b91231f8438b0d0b0bb4f17
jessi <jcm9472@rit.edu>
2018-10-14 10:48:12 -0400

    created new activities for each planned page of prototype

---
+++ app/src/main/java/com/example/salah/swen_101/EditMenu.java
@@ -0,0 +1,13 @@
+package com.example.salah.swen_101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_menu);
+    }
+}
