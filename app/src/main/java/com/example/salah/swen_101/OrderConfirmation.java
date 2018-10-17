3d67056df30aecd7feb7f7a57c3a2d81dd95c327
Dan Murray <dqm8494@rit.edu>
2018-10-17 10:40:16 -0400

    Did some cool stuff

--- app/src/main/java/com/example/salah/swen_101/OrderConfirmation.java
+++ app/src/main/java/com/example/salah/swen_101/OrderConfirmation.java
@@ -2,12 +2,31 @@ package com.example.salah.swen_101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OrderConfirmation extends AppCompatActivity {
+    private Button button;
+    private TextView cooking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirmation);
+
+        button = (Button) findViewById(R.id.ConfirmButton);
+        cooking = (TextView) findViewById(R.id.CookingText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cookingStart();
+            }
+        });
+    }
+
+    public void cookingStart(){
+        cooking.setTextColor(0xFF24CE24);
     }
 }
--- app/src/main/java/com/example/salah/swen_101/ViewMenu.java
+++ app/src/main/java/com/example/salah/swen_101/ViewMenu.java
@@ -1,19 +1,35 @@
 package com.example.salah.swen_101;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ViewMenu extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_menu);
 
+        button = (Button) findViewById(R.id.OrderButton);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openOrder();
+            }
+        });
+
+    }
+    public void openOrder(){
+        Intent intent = new Intent(this, OrderConfirmation.class);
+        startActivity(intent);
     }
 }
